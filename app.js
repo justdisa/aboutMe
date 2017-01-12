cf9bfebd7f2a05bb56ecdca485cdfa07a7d9d43f
Disa <justdisa@gmail.com>
2017-01-12 14:02:08 -0800

    fixed bug that caused crash before tally

--- app.js
+++ app.js
@@ -7,22 +7,19 @@ console.log('The user\'s name is: ' + user + '.');
 
 alert('Well, hello, ' + user + '. I\'m going to ask you some questions!');
 
-function questionOne() {
-  var answer = prompt('They\'ll all be yes or no questions, so just answer with a Y or an N. You got that, right?').toUpperCase();
-  console.log('The user answered: ' + answer);
+var answer = prompt('They\'ll all be yes or no questions, so just answer with a Y or an N. You got that, right?').toUpperCase();
+console.log('The user answered: ' + answer);
 
-  if (answer === 'Y' || answer === 'YES') {
-    alert('Marvelous, ' + user + '! Let\s go!');
-  } else if (answer === 'N' || answer === 'NO') {
-    alert('You know that entering N to tell me you didn\'t understand how to enter N showed me you understood how to enter N, right? Uh huh. Moving on, now.');
-  } else {
-    alert('Are you messing with me?');
-  }
+if (answer === 'Y' || answer === 'YES') {
+  alert('Marvelous, ' + user + '! Let\s go!');
+} else if (answer === 'N' || answer === 'NO') {
+  alert('You know that entering N to tell me you didn\'t understand how to enter N showed me you understood how to enter N, right? Uh huh. Moving on, now.');
+} else {
+  alert('Are you messing with me?');
 }
 
-function questionTwo() {
-  var answer1 = prompt('Okay. Next question. ' + user + ', I\'ve done some surprising things in my life. Let\'s start small. Do you think I\'ve ever fired a gun?').toUpperCase();
-  console.log('The user answered: ' + answer1);
+var answer1 = prompt('Okay. Next question. ' + user + ', I\'ve done some surprising things in my life. Let\'s start small. Do you think I\'ve ever fired a gun?').toUpperCase();
+console.log('The user answered: ' + answer1);
 
 if (answer1 === 'Y' || answer1 === 'YES') {
   alert('That\'s right! I fired a gun for the first time before I was two years old.');
@@ -33,10 +30,8 @@ if (answer1 === 'Y' || answer1 === 'YES') {
   alert('Yeah. Guns are a touchy subject. I wouldn\'t want to answer that one, either.');
 }
 
-function questionThree() {
-  var answer2 = prompt('Question number two. Or is it three? Do you think I\'ve ever been on a motorcycle?').toUpperCase();
-  console.log('The user answered: ' + answer2);
-
+var answer2 = prompt('Question number two. Or is it three? Do you think I\'ve ever been on a motorcycle?').toUpperCase();
+console.log('The user answered: ' + answer2);
 
 if (answer2 === 'Y' || answer2 === 'YES') {
   alert('I surely have! I had a Honda 50 Minibike when I was a kid. Wish I\'d kept that thing. People collect them, now.');
@@ -47,10 +42,8 @@ if (answer2 === 'Y' || answer2 === 'YES') {
   alert('I grew up around guns and motorcycles. Don\'t look so surprised.');
 }
 
-function questionFour() {
-  var answer3 = prompt('I sing--mostly for fun, but sometimes I get paid. I often tell a story about singing a duet on live television with Governor Gary Locke. Now, I know that performers have been known to exaggerate from time to time. Do you think I\'m making it up?').toUpperCase();
-  console.log('The user answered: ' + answer3);
-
+var answer3 = prompt('I sing--mostly for fun, but sometimes I get paid. I often tell a story about singing a duet on live television with Governor Gary Locke. Now, I know that performers have been known to exaggerate from time to time. Do you think I\'m making it up?').toUpperCase();
+console.log('The user answered: ' + answer3);
 
 if (answer3 === 'Y' || answer3 === 'YES') {
   alert('Who me? It\'s the absolute truth. I was singing a gospel duet at a public concert celebrating Martin Luther King Junior Day, and the Governor was in attendance. The guy I was singing with knew him pretty well. Without warning, he stuck the mic in the Governor\'s face. He sang.');
@@ -61,10 +54,8 @@ if (answer3 === 'Y' || answer3 === 'YES') {
   alert('It\'s true. My life is strange enough that I don\'t feel a need to make stuff up.');
 }
 
-function questionFive() {
-  var answer4 = prompt('You get on a plane and go somewhere. The plane lands. You disembark and trudge wearily through the airport. People do it a million times a day. Do you think I\'ve done it?').toUpperCase();
-  console.log('The user answered: ' + answer4);
-
+var answer4 = prompt('You get on a plane and go somewhere. The plane lands. You disembark and trudge wearily through the airport. People do it a million times a day. Do you think I\'ve done it?').toUpperCase();
+console.log('The user answered: ' + answer4);
 
 if (answer4 === 'Y' || answer4 === 'YES') {
   alert('Nope! I\'ve gotten on a plane before, but the plane I was on landed without me. I\'d already jumped out of it.');
@@ -75,10 +66,8 @@ if (answer4 === 'Y' || answer4 === 'YES') {
   alert('I have flown in a plane. Someday, I hope to land in one.');
 }
 
-function questionSix() {
-  var answer5 = prompt('Do you think I always wanted to learn to code?').toUpperCase();
-  console.log('The user answered: ' + answer5);
-
+var answer5 = prompt('Do you think I always wanted to learn to code?').toUpperCase();
+console.log('The user answered: ' + answer5);
 
 if (answer5 === 'Y' || answer5 === 'YES') {
   alert('You\'re right. I took a computer class in 1981 and I loved it, but I didn\'t have a computer at home. Besides, girls weren\'t supposed to like computers...right? That\'s what the boys in the computer lab told me. That derailed me for some time, but here I am. Funny how these things happen, isn\'t it, ' + user + '?');
@@ -114,7 +103,7 @@ for (index = 0; index < 4; index++) {
 
 for (index = 0; index < 6; index++) {
   var answer7 = prompt('Last question! You get six guesses. What state have I lived in other than Washington?').toUpperCase();
-  if (answer7 === 'OREGON' || answer7 === OR) {
+  if (answer7 === 'OREGON' || answer7 === 'OR') {
     alert ('That\s the one!');
     corAns.push(answer7);
     break;
